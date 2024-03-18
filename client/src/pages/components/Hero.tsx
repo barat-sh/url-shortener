7ecf95f26708b91a9d7c197eb0c59637e9453680
barat-sh <barath.elangovan@icloud.com>
2024-03-18 11:51:58 +0530

    UI signup, login, dashboard, cache interceptop in server

---
+++ client/src/pages/components/Hero.tsx
@@ -0,0 +1,128 @@
+import { alpha } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { dark } from '@mui/material/styles/createPalette';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Hero() {
+    const { id } = useParams();
+    const [longURL, setLongURL] = useState("");
+    const [shortURL, setShortURL] = useState("");
+
+    const handleGenerate = () => {
+        const url = `http://localhost:3005/urls/api/shortUrl/${id}`
+        console.log(longURL, id)
+    }
+
+  return (
+    <Box
+      id="hero"
+      sx={(theme) => ({
+        width: '100%',
+        backgroundImage:
+          theme.palette.mode === 'light'
+            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
+            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
+        backgroundSize: '100% 30%',
+        backgroundRepeat: 'no-repeat',
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', md: 'row' },
+              alignSelf: 'center',
+              textAlign: 'center',
+            }}
+          >
+            Our latest&nbsp;
+            <Typography
+              component="span"
+              variant="h4"
+              sx={{
+                color: (theme) =>
+                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+              }}
+            >
+              products
+            </Typography>
+          </Typography>
+          <Typography variant="body1" textAlign="center" color="text.secondary">
+            Explore our cutting-edge dashboard, delivering high-quality solutions
+            tailored to your needs. <br />
+            Elevate your experience with top-tier features and services.
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+          >
+            <TextField
+              id="outlined-basic"
+              hiddenLabel
+              size="small"
+              variant="outlined"
+              aria-label="Enter your URL"
+              placeholder="Enter your URL"
+              onChange={(event)=>{setLongURL(event.target.value)}}
+            />
+            <Button onClick={handleGenerate} size='small' variant="contained" color="primary">
+              generate
+            </Button>
+          </Stack>
+          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+            By clicking &quot;Start now&quot; you agree to our&nbsp;
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+            .
+          </Typography>
+        </Stack>
+        {/* <Box
+          id="image"
+          sx={(theme) => ({
+            mt: { xs: 8, sm: 10 },
+            alignSelf: 'center',
+            height: { xs: 200, sm: 700 },
+            width: '100%',
+            backgroundImage:
+              theme.palette.mode === 'light'
+                ? 'url("/static/images/templates/templates-images/hero-light.png")'
+                : 'url("/static/images/templates/templates-images/hero-dark.png")',
+            backgroundSize: 'cover',
+            borderRadius: '10px',
+            outline: '1px solid',
+            outlineColor:
+              theme.palette.mode === 'light'
+                ? alpha('#BFCCD9', 0.5)
+                : alpha('#9CCCFC', 0.1),
+            boxShadow:
+              theme.palette.mode === 'light'
+                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
+                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+          })}
+        /> */}
+      </Container>
+    </Box>
+  );
+}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { UrlsModule } from './urls/urls.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, UrlsModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UsersModule,
+    UrlsModule,
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 3600000,
+    }),
+  ],
 })
 export class AppModule {}
